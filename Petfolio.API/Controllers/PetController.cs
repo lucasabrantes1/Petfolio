993ff0958fd4e5eda7cb11adf6b3423d353ba5a2
lucasabrantes1 <lucasabrantes002@gmail.com>
2024-11-20 13:07:03 -0300

    Endpoint de edição

--- Petfolio.API/Controllers/PetController.cs
+++ Petfolio.API/Controllers/PetController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Petfolio.Application.UseCases.Pet.Register;
+using Petfolio.Application.UseCases.Pet.Update;
 using Petfolio.Communication.Requests;
 using Petfolio.Communication.Responses;
 
@@ -10,10 +11,22 @@ public class PetController : ControllerBase
 {
     [HttpPost]
     [ProducesResponseType(typeof(ResponseRegisteredPetJson),StatusCodes.Status201Created)]
-    public IActionResult Register([FromBody] RequestRegisterPetJson request)
+    public IActionResult Register([FromBody] RequestPetJson request)
     {   
         //Business logic
         var response = new RegisterPetUseCase().Execute(request);
         return Created(string.Empty, response);
+    }
+
+
+    [HttpPut]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult Update([FromRoute]int id, [FromBody] RequestPetJson request) 
+    {   
+        var useCase = new UpdatePetUseCase();
+        useCase.Execute(id, request);
+
+        return NoContent();
     }
 }
--- Petfolio.Communication/Requests/RequestPetJson.cs
+++ Petfolio.Communication/Requests/RequestPetJson.cs
@@ -1,6 +1,6 @@
 ﻿using Petfolio.Communication.Enums;
 namespace Petfolio.Communication.Requests;
-public class RequestRegisterPetJson
+public class RequestPetJson
 {
     public string Name { get; set; } = string.Empty;
     public DateTime Birthday { get; set; }
